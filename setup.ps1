c1482c5c3a628b90ca6c906c170568e756eaa239
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2017-10-04 12:47:07 -0400

    setup.ps1 enhanced with suppressbootstrap switch and depth 1 invocation of setup.ps1 -suppressbootstrap, plus better verbose output.

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,12 @@
-﻿write-verbose "Setup..." -Verbose
+﻿PARAM([Switch]$SuppressBootstrap)
+write-verbose "Setup (running $PsCommandPath)..." -Verbose
+
+If (-not $SuppressBootstrap.IsPresent) {
 write-verbose "Running bootstrap..." -Verbose
 . .\bootstrap.ps1
+}
+ELSE {write-verbose "Bootstrap suppressed." -Verbose}
+
+Get-ChildItem -Directory | Get-ChildItem -filter setup.ps1 | ForEach-Object { Push-Location; Set-Location (Split-Path -Parent $_.Fullname); . ".\$($_.Name)" -SuppressBootstrap ; Pop-Location }
+
+write-verbose "Done ($PsCommandPath)." -Verbose
